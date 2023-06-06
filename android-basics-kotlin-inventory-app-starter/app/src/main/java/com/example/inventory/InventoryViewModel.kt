59aebdf2bf8e51d9877bfc4e4e364fc2d4d7aa09
Luca Nicoletti <luca.nicolett@gmail.com>
2023-06-06 15:33:49 +0100

    Add a view model

---
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.inventory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    private fun getNewItemEntry(name: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            itemName = name,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt(),
+        )
+    }
+
+    fun addNewItem(name: String, itemPrice: String, itemCount: String) {
+        insertItem(getNewItemEntry(name, itemPrice, itemCount))
+    }
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown viewmodel class")
+    }
+}
--- android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/Item.kt
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/Item.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "item")
 data class Item(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "name") val itemName: String,
     @ColumnInfo(name = "price") val itemPrice: Double,
     @ColumnInfo(name = "quantity") val quantityInStock: Int,
